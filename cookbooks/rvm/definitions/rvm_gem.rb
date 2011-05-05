3831d67fc37a72067d23449ec0116fc55df408f1
Scott McLeod <scott.mcleod@theice.com>
2011-05-04 22:55:06 -0400

    Add redmine cookbook

--- cookbooks/rvm/definitions/rvm_gem.rb
+++ cookbooks/rvm/definitions/rvm_gem.rb
@@ -1,10 +1,11 @@
 # removes the full path to gem so rvm can take care of which gem binary to use
 
-define :rvm_gem, :source => nil, :version => nil do
+define :rvm_gem, :source => nil, :version => nil, :options => nil do
   include_recipe "rvm"
   gem_package params[:name] do
     gem_binary "gem"
     source params[:source] if params[:source]
     version params[:version] if params[:version]
+    options params[:options] if params[:options]
   end
 end
